b4124a8f022c53e3fc8c2193b5754d9aa19a6140
olleaxelssson <hk8dxgs5n5@privaterelay.appleid.com>
2026-02-17 12:44:17 +0100

    Test for JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const buttonElement = document.getElementById("add-to-cart-btn");
+const cartElement = document.getElementById("cart");
 
 buttonElement.addEventListener("click", function (event) {
   alert("Item added to cart!");
